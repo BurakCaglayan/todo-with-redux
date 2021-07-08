7ff941ba5ecefefbecdc55a3629f45d03a41f737
BurakCaglayan <burakkcaglayann@gmail.com>
2021-07-08 12:41:28 +0300

    Add Create and Remove actions

--- src/redux/actions/todo.js
+++ src/redux/actions/todo.js
@@ -12,3 +12,27 @@ export const list = () => (dispatch) => {
       })
     );
 };
+
+export const create = (data) => (dispatch) => {
+  dispatch({
+    type: "todo/pending",
+  });
+
+  fetch("http://localhost:5000/api/todos", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify(data),
+  }).then(() => dispatch(list()));
+};
+
+export const remove = (id) => (dispatch) => {
+  dispatch({
+    type: "todo/pending",
+  });
+
+  fetch(`http://localhost:5000/api/todos/${id}`, {
+    method: "DELETE",
+  }).then(() => dispatch(list()));
+};
